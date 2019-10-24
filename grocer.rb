a9ae15844e8bb90bb4e08cfc0f378a99979bf3c6
remi.odufuye <remi.odufuye@gmail.com>
2019-10-24 13:46:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,36 +1,23 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-end
+require 'pry' 
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-end
+    final_hash = {}
+    cart.each do |element_hash|
+      element_name = element_hash.keys[0]
+      element_stats = element_hash.values[0]
+  
 
-def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
-
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
-
-def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
-end
+      if final_hash.has_key?[element_name]
+        final_hash[element_name][:count] += 1 
+      
+      else
+        final_hash[element_name] = {
+          count: 1 , 
+          price: = element_stats[:price] , 
+          clearance: = element_stats[:clearance] 
+        }
+      #  binding.pry 
+      end
+    end 
+    final_hash
+end  
