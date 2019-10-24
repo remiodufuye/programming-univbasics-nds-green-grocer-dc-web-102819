05fb960504bf10e4dee22d7da0d1adc946d95d6f
remi.odufuye <remi.odufuye@gmail.com>
2019-10-24 14:11:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,15 @@
 
 require 'pry'
 
+def find_item_by_name_in_collection(name, collection)
+  i = 0
+  while i < collection.length do
+    return collection[i] if name == collection[i][:item]
+    i += 1
+  end
+nil
+end
+
 def consolidate_cart(cart)
     final_hash = {}
     cart.each do |element_hash|
@@ -19,3 +28,28 @@ def consolidate_cart(cart)
     end 
     final_hash
 end  
+
+
+def apply_coupons(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
+end
+
+def apply_clearance(cart)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
+end
+
+def checkout(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # This method should call
+  # * consolidate_cart
+  # * apply_coupons
+  # * apply_clearance
+  #
+  # BEFORE it begins the work of calculating the total (or else you might have
+  # some irritated customers
+end
